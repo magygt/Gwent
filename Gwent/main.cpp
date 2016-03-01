0bdf4a2bbe1918d0976cb0267a2450eb8e71dccf
magygt <magygt@gmail.com>
2016-03-01 12:45:36 +0800

    Initial Commit

---
+++ Gwent/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gwent
+//
+//  Created by Magygt on 3/1/16.
+//  Copyright © 2016 Magygt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
